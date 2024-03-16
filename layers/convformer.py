edde1e73821494371ead29497aa9ab0a69513bef
Saurav Sharma <75607847+sauravs0@users.noreply.github.com>
2024-03-16 15:43:37 +0530

    Added Dual Attention in convformer

--- layers/convformer.py
+++ layers/convformer.py
@@ -1,12 +1,121 @@
-import tensorflow as tf 
+import torch
+from torch import nn
+import numpy as np
+import torch
+import math
+from torch.nn import Module, Sequential, Conv2d, ReLU,AdaptiveMaxPool2d, AdaptiveAvgPool2d, \
+    NLLLoss, BCELoss, CrossEntropyLoss, AvgPool2d, MaxPool2d, Parameter, Linear, Sigmoid, Softmax, Dropout, Embedding
+from torch.nn import functional as F
+from torch.autograd import Variable
+
+import torch
+from torch import nn
+import numpy as np
+import torch
+import math
+from torch.nn import Module, Sequential, Conv2d, ReLU,AdaptiveMaxPool2d, AdaptiveAvgPool2d, \
+    NLLLoss, BCELoss, CrossEntropyLoss, AvgPool2d, MaxPool2d, Parameter, Linear, Sigmoid, Softmax, Dropout, Embedding
+from torch.nn import functional as F
+from torch.autograd import Variable
+
+from os.path import join as pjoin
+from collections import OrderedDict
+
+import torch.nn.functional as F
+
+from torch.nn import Module, Sequential, Conv2d, ReLU,AdaptiveMaxPool2d, AdaptiveAvgPool2d, \
+    NLLLoss, BCELoss, CrossEntropyLoss, AvgPool2d, MaxPool2d, Parameter, Linear, Sigmoid, Softmax, Dropout, Embedding
+from torch.nn import functional as F
+from torch.autograd import Variable
+
+class PAM_Module(Module):
+    """ Position attention module"""
+    #Ref from SAGAN
+    def __init__(self, in_dim):
+        super(PAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+        self.query_conv = Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.key_conv = Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.value_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = Parameter(torch.zeros(1))
+
+        self.softmax = Softmax(dim=-1)
+    def call(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X (HxW) X (HxW)
+        """
+        m_batchsize, C,height, width = x.size()
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width*height).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width*height)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width*height)
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, height, width)
+
+        out = self.gamma*out + x
+        return out
+
+class CAM_Module(Module):
+    """ Channel attention module"""
+    def __init__(self, in_dim):
+        super(CAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+
+        self.gamma = Parameter(torch.zeros(1))
+        self.softmax  = Softmax(dim=-1)
+    def call(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X C X C
+        """
+        m_batchsize, C, height, width = x.size()
+        proj_query = x.view(m_batchsize, C, -1)
+        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1)
+        energy = torch.bmm(proj_query, proj_key)
+        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
+        attention = self.softmax(energy_new)
+        proj_value = x.view(m_batchsize, C, -1)
+
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, C, height, width)
+
+        out = self.gamma*out + x
+        return out
+
+
 
 def convformer(input_tensor, filters, padding = "same"):
     
     x = tf.keras.layers.LayerNormalization()(input_tensor)
     x = tf.keras.layers.SeparableConv2D(filters, kernel_size = (3,3), padding = padding)(x)
-    # x = x1 + x2 + x3
-    x = tf.keras.layers.Attention()([x, x, x])
-    out = tf.keras.layers.Add()([x, input_tensor])
+
+    # to use simple attention from tensorflow keras
+    # x = tf.keras.layers.Attention()([x, x, x])
+    # out = tf.keras.layers.Add()([x, input_tensor])
+
+    # to use dual attention: channel attention, position attention
+    pam = PAM_Module(filters)(x)
+    cam = CAM_Module(filters)(x)
+    dual_attention = tf.keras.layers.Add()([pam, cam])
+    out = tf.keras.layers.Add()([dual_attention, input_tensor])
+
+    # to use multiheadattention
+    # Set num_heads to a suitable value, commonly 4 or 8 or 16. Experiment with different settings for optimal performance.
+    # Example number of heads, adjust as needed
+    # num_heads = 4  
+    # x = tf.keras.layers.MultiHeadAttention(num_heads=num_heads)(x, x, x)
+    # out = tf.keras.layers.Add()([x, input_tensor])
     
     x1 = tf.keras.layers.Dense(filters, activation = "gelu")(out)
     x1 = tf.keras.layers.Dense(filters)(x1)
